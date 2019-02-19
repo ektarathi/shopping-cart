7a6829ac388bb8fae4d4b9fb25689fabaa9f66f3
ekta-rathi <ekta.rathi@oxfordcc.co.uk>
2019-02-19 22:00:12 +0000

    First Test Case

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import { Route, Switch } from "react-router-dom";
 import "./App.scss";
 import Product from "./components/Product";
 import ProductDetails from "./components/ProductDetails";
+import NotFoundPage from "./components/NotFound";
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Switch>
           <Route path="/:name/:id" component={ProductDetails} />
-          <Route path="/" component={Product} />
+          <Route path="/" exact component={Product} />
+          <Route component={NotFoundPage} />
         </Switch>
       </div>
     );
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { mount, configure } from "enzyme";
+import { MemoryRouter } from "react-router-dom";
+import Adapter from "enzyme-adapter-react-16";
+import Product from "./components/Product";
+import NotFoundPage from "./components/NotFound";
 import App from "./App";
 
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+configure({ adapter: new Adapter() });
+
+test("invalid path should redirect to 404", () => {
+  const wrapper = mount(
+    <MemoryRouter initialEntries={["/random"]}>
+      <App />
+    </MemoryRouter>
+  );
+  expect(wrapper.find(Product)).toHaveLength(0);
+  expect(wrapper.find(NotFoundPage)).toHaveLength(1);
+});
+
+test("valid path should not redirect to 404", () => {
+  const wrapper = mount(
+    <MemoryRouter initialEntries={["/"]}>
+      <App />
+    </MemoryRouter>
+  );
+  expect(wrapper.find(Product)).toHaveLength(1);
+  expect(wrapper.find(NotFoundPage)).toHaveLength(0);
 });
