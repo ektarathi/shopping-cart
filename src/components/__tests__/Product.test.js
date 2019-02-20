7a437112304673774d59919b4469badb5f716f35
ekta-rathi <ekta.rathi@oxfordcc.co.uk>
2019-02-20 12:58:44 +0000

    updated test case

--- src/components/__tests__/Product.test.js
+++ src/components/__tests__/Product.test.js
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
-import { shallow, configure } from "enzyme";
+import { shallow, configure, mount } from "enzyme";
 import Product from "../Product";
 import Adapter from "enzyme-adapter-react-16";
 configure({ adapter: new Adapter() });
 
+const props = {
+  error: null,
+  isLoaded: true
+};
 describe("Product", () => {
+  let data;
   it("fetch correctly", async () => {
-    let data;
     const response = await fetch(
       "https://api.johnlewis.com/v1/products/search?q=dishwasher&key=Wu1Xqn3vNrd1p7hqkvB6hEu0G9OrsYGb&pageSize=20"
     );
@@ -16,11 +20,13 @@ describe("Product", () => {
   });
 
   it("should render correctly ", () => {
-    const wrapper = shallow(<Product isLoaded={true} />);
+    console.log("data", data);
+    const wrapper = mount(<Product {...props} />);
     const product = wrapper.find(".product");
     console.log(wrapper.debug());
-    expect(product).toHaveLength(0);
-    const product__item = wrapper.find(".product__item");
-    expect(product__item).toHaveLength(0);
+    expect(product).toHaveLength(1);
+    //const product__item = wrapper.find(".product__item");
+    //expect(product__item).toHaveLength(0);
+    //expect(wrapper.find(".product__item__image").exists()).toBe(true);
   });
 });
